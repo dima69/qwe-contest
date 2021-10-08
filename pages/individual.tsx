3091837e0ac2084da3838721177aa3c1ecb445a3
dimadima <23dima6@gmail.com>
2021-10-09 01:07:21 +0300

    add basic leaderboard and individual pages

--- pages/individual.tsx
+++ pages/individual.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-const Home: NextPage = () => {
+const Individual: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Individual ratings</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="bg-gray-50">
@@ -33,22 +33,7 @@ const Home: NextPage = () => {
       </header>
 
       <main>
-        {/* <div className="flex flex-wrap max-w-7xl mx-auto">
-          <div className="relative min-w-max w-1/2 h-96">
-            <Image src="/me.jpg" layout="fill" objectFit="scale-down" />
-          </div>
-          <div className="relative min-w-max w-1/2 h-96">
-            <Image src="/me1.jpg" layout="fill" objectFit="scale-down" />
-          </div>
-        </div> */}
-        <div className="grid grid-rows-2 grid-cols-1 md:grid-cols-2 md:grid-rows-1">
-          <div className="relative h-96 min-w-full my-2">
-            <Image src="/me.jpg" layout="fill" objectFit="scale-down" className="hover:brightness-95 hover:cursor-pointer" />
-          </div>
-          <div className="relative h-96 min-w-full my-2">
-            <Image src="/me1.jpg" layout="fill" objectFit="scale-down" className="hover:brightness-95 hover:cursor-pointer" />
-          </div>
-        </div>
+        <span className="text-center">Individual page</span>
       </main>
 
       <footer className="flex items-center justify-center w-full h-10 bg-gray-200">
@@ -58,4 +43,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default Individual;
