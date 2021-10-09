079db4d75bed8af6b22cef5fd6a0b94f00af1ca7
dimadima <23dima6@gmail.com>
2021-10-10 01:21:45 +0300

    move navigation bar and footer to components

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="flex items-center justify-center w-full h-10 bg-gray-200">
+            <span>@@@footer text with icon</span>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ components/NavigationBar.tsx
@@ -0,0 +1,29 @@
+const NavigationBar = () => {
+    return (
+        // max width and items between and center
+        <header className="bg-gray-50">
+            <div className="flex text-3xl max-w-3xl justify-between mx-auto">
+                <a
+                    className="text-blue-500 hover:bg-green-50 py-2 px-5 rounded-md hover:shadow-md hover:text-green-600"
+                    href="/"
+                >
+                    Face-to-face
+                </a>
+                <a
+                    className="text-blue-500 hover:bg-pink-50 py-2 px-5 rounded-md hover:shadow-md hover:text-pink-600"
+                    href="/individual"
+                >
+                    Individual
+                </a>
+                <a
+                    className="text-blue-500 hover:bg-yellow-50 py-2 px-5 rounded-md hover:shadow-md hover:text-yellow-600"
+                    href="/leaderboard"
+                >
+                    Leaderboard
+                </a>
+            </div>
+        </header>
+    )
+};
+
+export default NavigationBar;
