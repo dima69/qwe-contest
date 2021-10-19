f5024504976d215b151aeabb8c1419af8f0372f4
dimadima <23dima6@gmail.com>
2021-10-19 20:13:58 +0300

    add basic API routes

--- pages/api/getOne.ts
+++ pages/api/getOne.ts
@@ -2,12 +2,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  pic_id: number,
+  comments: string[],
+  average_score: number,
+  image_url: string,
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).json({
+    pic_id: 1,
+    comments: ['good looking', 'handsome'],
+    average_score: 4.3,
+    image_url: 'me.jpg'
+  })
+};
