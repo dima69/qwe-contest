8a09aa2df3c8e5272d9dca8de58dbe60711e2a46
dimadima <23dima6@gmail.com>
2021-11-13 15:47:36 +0300

    add basic getPair API endpoint

---
+++ pages/api/getPair.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const directory = './public/';
+  let files_list = fs.readdirSync(directory);  
+  let response = [];
+  const shuffled_array = files_list.sort(() => 0.5 - Math.random());
+  let two_selected_items = shuffled_array.slice(0, 2);
+  for (const filename of two_selected_items) {
+    // @@@
+    response.push({
+      user_id: filename.slice(0, 1),
+      image_url: filename,
+    })
+  }
+  res.status(200).json(response)
+};
